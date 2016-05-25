e8ff4c1f3bfe26cff314354175d480872ac1098c
Kevin Ng <kevin@kevinng.ca>
2016-05-25 16:42:16 -0400

    Added gulp

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+gulp.task('styles', function(){
+	return gulp.src('./dev/styles/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+		.pipe(concat('style.css'))
+		.pipe(gulp.dest('./public/styles/'))
+		.pipe(reload({stream: true}));
+});
+
+gulp.task('watch', function(){
+	gulp.watch('./dev/styles/**/*.scss', ['styles']);
+	gulp.watch('./public/*.html', reload);
+});
+
+gulp.task('browser-sync', function(){
+	browserSync.init({
+		server: './public'
+	})
+});
+
+gulp.task('default', ['browser-sync', 'styles', 'watch']);
