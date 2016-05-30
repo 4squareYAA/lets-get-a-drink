fb824eb58cce8fba4b075562a21259e8524d6ca5
Kevin Ng <kevin@kevinng.ca>
2016-05-30 11:08:24 -0400

    ghpages commit

--- gulpfile.js
+++ gulpfile.js
@@ -10,26 +10,26 @@ gulp.task('styles', function(){
 		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
 		.pipe(concat('style.css'))
-		.pipe(gulp.dest('./public/styles/'))
+		.pipe(gulp.dest('./styles/'))
 		.pipe(reload({stream: true}));
 });
 
 gulp.task('scripts', function(){
 	gulp.src('./dev/js/*.js')
-		.pipe(gulp.dest('./public/js'))
+		.pipe(gulp.dest('./js'))
 		.pipe(reload({stream: true}));
 });
 
 gulp.task('browser-sync', function(){
 	browserSync.init({
-		server: './public'
+		server: '.'
 	})
 });
 
 gulp.task('watch', function(){
 	gulp.watch('./dev/styles/**/*.scss', ['styles']);
 	gulp.watch('./dev/js/*.js', ['scripts']);
-	gulp.watch('./public/*.html', reload);
+	gulp.watch('.*.html', reload);
 });
 
 gulp.task('default', ['browser-sync', 'styles', 'scripts', 'watch']);
